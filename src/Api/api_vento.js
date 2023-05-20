374b7c72fd7879c757dd743a837460cb131eadb8
Flavio-LP <flavio5070@outlook.com>
2023-05-20 13:53:35 -0300

    refatoração api

--- src/Api/api_vento.js
+++ src/Api/api_vento.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: "https://tccbackend-production.up.railway.app/vento",
 });
 
+api.use(function (req, res, next) { //allow cross origin requests
+  res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 export default api;
